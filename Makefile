09455e7552bac28b4279f589752e5754a4054cbc
solinguo <552002499@qq.com>
2017-08-05 11:35:10 +0800

    add stm lib

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 
 TARGET = bootloader
 
-CROSS_COMPILE     ?= arm-none-eabi-
+LIB = lib_periph_driver.a
+
+SRC = start_up.o main.o
+SRC += $(LIB)
 
 ############### Compilation configuration ################
+CROSS_COMPILE     ?= arm-none-eabi-
+
 AS = $(CROSS_COMPILE)as
 CC = $(CROSS_COMPILE)gcc
 LD = $(CROSS_COMPILE)ld
@@ -13,19 +18,20 @@ OBJCOPY = $(CROSS_COMPILE)objcopy
 PROCESSOR = -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16
 
 INCLUDES = -Iinclude -I./
-
+INCLUDES += -I ./st_lib/STM32F4xx_StdPeriph_Driver/inc
+INCLUDES += -I ./st_lib/CMSIS/stm32f4xx/inc
+INCLUDES += -I ./st_lib/CMSIS/inc
 
 CFLAGS += $(PROCESSOR) $(INCLUDES)
 CFLAGS += -Wall -fno-strict-aliasing 
 CFLAGS += -O0 -g3
-
+CFLAGS += -DSTM32F40_41xxx
 
 ASFLAGS = $(PROCESSOR) $(INCLUDES)
 
 LDFLAGS = -Mmap > $(TARGET).map
 LDFLAGS += -T ./cm4/bootloader.ld
 
-ALL = start_up.o main.o
 
 $(TARGET).bin:$(TARGET).elf
 	$(OBJCOPY) -O binary $(TARGET).elf $(TARGET).bin
@@ -36,8 +42,14 @@ start_up.o:./cm4/start_up.S
 main.o:main.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
-$(TARGET).elf:$(ALL)
-	$(LD) $(LDFLAGS) $(ALL) --output $(TARGET).elf
+$(TARGET).elf:$(SRC)
+	$(LD) $(LDFLAGS) $(SRC) --output $(TARGET).elf
+
+lib_periph_driver.a:
+	$(MAKE) -C st_lib/STM32F4xx_StdPeriph_Driver/src/
+
 
 clean:
-	rm -f *.o *.map *.elf *.bin *.obj
+	rm -f *.o *.map *.elf *.bin *.obj *.a
+	$(MAKE) clean -C st_lib/STM32F4xx_StdPeriph_Driver/src/
+
--- main.c
+++ main.c
@@ -1,86 +1,42 @@
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
 
 
-#define __IO volatile
-#define uint32_t unsigned long
-#define uint16_t unsigned int
-
-typedef struct
+void LED_Init(void)
 {
-  __IO uint32_t CR;            /*!< RCC clock control register,                                  Address offset: 0x00 */
-  __IO uint32_t PLLCFGR;       /*!< RCC PLL configuration register,                              Address offset: 0x04 */
-  __IO uint32_t CFGR;          /*!< RCC clock configuration register,                            Address offset: 0x08 */
-  __IO uint32_t CIR;           /*!< RCC clock interrupt register,                                Address offset: 0x0C */
-  __IO uint32_t AHB1RSTR;      /*!< RCC AHB1 peripheral reset register,                          Address offset: 0x10 */
-  __IO uint32_t AHB2RSTR;      /*!< RCC AHB2 peripheral reset register,                          Address offset: 0x14 */
-  __IO uint32_t AHB3RSTR;      /*!< RCC AHB3 peripheral reset register,                          Address offset: 0x18 */
-  uint32_t      RESERVED0;     /*!< Reserved, 0x1C                                                                    */
-  __IO uint32_t APB1RSTR;      /*!< RCC APB1 peripheral reset register,                          Address offset: 0x20 */
-  __IO uint32_t APB2RSTR;      /*!< RCC APB2 peripheral reset register,                          Address offset: 0x24 */
-  uint32_t      RESERVED1[2];  /*!< Reserved, 0x28-0x2C                                                               */
-  __IO uint32_t AHB1ENR;       /*!< RCC AHB1 peripheral clock register,                          Address offset: 0x30 */
-  __IO uint32_t AHB2ENR;       /*!< RCC AHB2 peripheral clock register,                          Address offset: 0x34 */
-  __IO uint32_t AHB3ENR;       /*!< RCC AHB3 peripheral clock register,                          Address offset: 0x38 */
-  uint32_t      RESERVED2;     /*!< Reserved, 0x3C                                                                    */
-  __IO uint32_t APB1ENR;       /*!< RCC APB1 peripheral clock enable register,                   Address offset: 0x40 */
-  __IO uint32_t APB2ENR;       /*!< RCC APB2 peripheral clock enable register,                   Address offset: 0x44 */
-  uint32_t      RESERVED3[2];  /*!< Reserved, 0x48-0x4C                                                               */
-  __IO uint32_t AHB1LPENR;     /*!< RCC AHB1 peripheral clock enable in low power mode register, Address offset: 0x50 */
-  __IO uint32_t AHB2LPENR;     /*!< RCC AHB2 peripheral clock enable in low power mode register, Address offset: 0x54 */
-  __IO uint32_t AHB3LPENR;     /*!< RCC AHB3 peripheral clock enable in low power mode register, Address offset: 0x58 */
-  uint32_t      RESERVED4;     /*!< Reserved, 0x5C                                                                    */
-  __IO uint32_t APB1LPENR;     /*!< RCC APB1 peripheral clock enable in low power mode register, Address offset: 0x60 */
-  __IO uint32_t APB2LPENR;     /*!< RCC APB2 peripheral clock enable in low power mode register, Address offset: 0x64 */
-  uint32_t      RESERVED5[2];  /*!< Reserved, 0x68-0x6C                                                               */
-  __IO uint32_t BDCR;          /*!< RCC Backup domain control register,                          Address offset: 0x70 */
-  __IO uint32_t CSR;           /*!< RCC clock control & status register,                         Address offset: 0x74 */
-  uint32_t      RESERVED6[2];  /*!< Reserved, 0x78-0x7C                                                               */
-  __IO uint32_t SSCGR;         /*!< RCC spread spectrum clock generation register,               Address offset: 0x80 */
-  __IO uint32_t PLLI2SCFGR;    /*!< RCC PLLI2S configuration register,                           Address offset: 0x84 */
-  __IO uint32_t PLLSAICFGR;    /*!< RCC PLLSAI configuration register,                           Address offset: 0x88 */
-  __IO uint32_t DCKCFGR;       /*!< RCC Dedicated Clocks configuration register,                 Address offset: 0x8C */
+  	GPIO_InitTypeDef  GPIO_InitStructure;
 
-} RCC_TypeDef;
-
-#define RCC ((RCC_TypeDef *)0x40023800)
-
-#define PERIPH_BASE           ((unsigned int)0x40000000)
-
-#define AHB1PERIPH_BASE       (PERIPH_BASE + 0x00020000)
-
-#define GPIOA_BASE            (AHB1PERIPH_BASE + 0x0000)
-#define GPIOB_BASE            (AHB1PERIPH_BASE + 0x0400)
+  	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+  	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  	GPIO_Init(GPIOB, &GPIO_InitStructure);
+}
 
 
-typedef struct
+void delay(unsigned long i)
 {
-  __IO uint32_t MODER;    /*!< GPIO port mode register,               Address offset: 0x00      */
-  __IO uint32_t OTYPER;   /*!< GPIO port output type register,        Address offset: 0x04      */
-  __IO uint32_t OSPEEDR;  /*!< GPIO port output speed register,       Address offset: 0x08      */
-  __IO uint32_t PUPDR;    /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
-  __IO uint32_t IDR;      /*!< GPIO port input data register,         Address offset: 0x10      */
-  __IO uint32_t ODR;      /*!< GPIO port output data register,        Address offset: 0x14      */
-  __IO uint16_t BSRRL;    /*!< GPIO port bit set/reset low register,  Address offset: 0x18      */
-  __IO uint16_t BSRRH;    /*!< GPIO port bit set/reset high register, Address offset: 0x1A      */
-  __IO uint32_t LCKR;     /*!< GPIO port configuration lock register, Address offset: 0x1C      */
-  __IO uint32_t AFR[2];   /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
-} GPIO_TypeDef;
-
-
-#define GPIOA ((GPIO_TypeDef *)GPIOA_BASE)
-#define GPIOB ((GPIO_TypeDef *)GPIOB_BASE)
-
-#define LED_PIN 9
-
+	unsigned long j;
+	while (i--) {
+		for (j = i; j > 0; j--) {
+			if (j == 10)
+				j--;
+		}
+	}
+}
 
 int main()
 {
-	RCC->AHB1ENR |= 1 << 1;
+	LED_Init();
 
-    GPIOB->MODER = 0x55555555;
+	while (1) {
+		GPIOB->ODR = 0x0000;
+		delay(1000);
+		GPIOB->ODR = 0xFFFF;
+		delay(1000);
+	}
 
-	GPIOB->ODR = 0x0000;
-
-	while(1)
-		;
 	return 0;
 }
